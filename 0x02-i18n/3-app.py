80d7f44ee9d2029b25fd776bec6e8661c00c1a19
koome <koomemc@gmail.com>
2024-04-09 21:47:30 +0300

    use gettext

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Babel configuration file"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config():
+    """
+    configuration class
+    attributes:
+        LANGUAGES: list
+    """
+    LANGUAGES = ["en", "fr"]
+
+
+app.config.from_object('1-app.Config')
+
+
+@babel.localeselector
+def get_locale():
+    """
+    get locale from request
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def index():
+    """
+    render index template
+    """
+    return render_template("2-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
